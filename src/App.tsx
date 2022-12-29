f53ad0ddf6bcccc5633406d8ab48edcd58ff0992
mouravocal <diego.moura3@gmail.com>
2022-12-28 23:06:38 -0300

    feat: scrolling chords page

---
+++ src/App.tsx
@@ -0,0 +1,191 @@
+import { StatusBar } from 'expo-status-bar';
+import { useRef, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { WebView } from 'react-native-webview'
+import { useInterval } from './hooks/useInterval';
+
+export default function App() {
+  const [isScrolling, setIsScrolling] = useState(false)
+  const [delay, setDelay] = useState(null)
+
+  const webViewRef = useRef<WebView>(null)
+
+  const scroll = () => {
+    webViewRef.current.injectJavaScript(`
+      window.scrollBy(0,50)
+    `)
+  }
+
+  useInterval(scroll, isScrolling ? delay : null)
+
+  const tab = `<pre>[Intro] <b class="js-modal-trigger">Dm</b>  <b class="js-modal-trigger">Bb5(9)</b>  <b>C</b>  <b class="js-modal-trigger">Dm</b>
+
+  [Primeira Parte]
+  
+        <b>Dm</b>
+  Quem sabe eu ainda sou
+           <b>C</b>
+  Uma garotinha
+  <b>Gm</b>                         <b>Dm</b>
+    Esperando o ônibus da escola sozinha
+                                  <b>C</b>
+  Cansada com minhas meias três quartos
+  <b class="js-modal-trigger">Gm</b>                     <b>Dm</b>
+    Rezando baixo pelos cantos
+       <b>C</b>              <b>Bb5(9)</b>
+  Por ser uma menina má
+  <b>Dm</b>                                  <b>C</b>
+    Quem sabe o príncipe virou um chato
+  <b>Gm</b>                       <b>Dm</b>
+    Que vive dando no meu saco
+        <b>C</b>                    <b>Bb5(9)</b>
+  Quem sabe a vida é não sonhar
+  
+  [Refrão]
+  
+  <b>Dm</b>              <b>C</b>
+    Eu só peço a Deus
+            <b class="js-modal-trigger">F</b>        <b>G</b>
+  Um pouco de malandragem
+  <b>Dm</b>            <b>C</b>
+    Pois sou criança
+             <b>F</b>         <b>G</b>
+   E não conheço a verdade
+  <b>Bb5(9)</b>         <b>C</b>                     <b>Dm</b>
+        Eu sou poeta e não aprendi a amar
+  <b>Bb5(9)</b>         <b>C</b>                     <b>Dm</b>
+        Eu sou poeta e não aprendi a amar
+  
+  [Segunda Parte]
+  
+                            <b>C</b>
+  Bobeira não viver a realidade
+  <b>Gm</b>                              <b>Dm</b>
+    E eu ainda tenho uma tarde inteira
+  <b>G</b>              <b>F</b>   <b class="js-modal-trigger">C</b>              <b>Dm</b>
+    Eu ando nas ruas   eu troco o cheque
+  <b>Bb5(9)</b>                      <b>Dm</b>
+        Mudo uma planta de lugar
+  <b>G</b>               <b>F</b>    <b>C</b>               <b>Dm</b>
+    Dirijo o meu carro,  tomo o meu pileque
+  <b>Bb5(9)</b>                            <b>Dm</b>
+        E ainda tenho tempo pra cantar
+  
+  [Refrão]
+  
+                <b>C</b>
+  Eu só peço a Deus
+            <b>F</b>        <b>G</b>
+  Um pouco de malandragem
+  <b>Dm</b>            <b>C</b>
+    Pois sou criança
+            <b>F</b>         <b>G</b>
+  E não conheço a verdade
+  <b>Bb5(9)</b>         <b>C</b>                     <b>Dm</b>
+        Eu sou poeta e não aprendi a amar
+  <b>Bb5(9)</b>         <b>C</b>                     <b>Dm</b>
+        Eu sou poeta e não aprendi a amar
+  
+  [Solo] <b>C</b>  <b>Gm</b>  <b>Dm</b>
+  
+  [Segunda Parte]
+  
+  <b>G</b>              <b>F</b>   <b>C</b>              <b>Dm</b>
+    Eu ando nas ruas   eu troco o cheque
+  <b>Bb5(9)</b>                      <b>Dm</b>
+        Mudo uma planta de lugar
+  <b>G</b>               <b>F</b>    <b>C</b>               <b>Dm</b>
+    Dirijo o meu carro,  tomo o meu pileque
+  <b>Bb5(9)</b>                            <b>Dm</b>
+        E ainda tenho tempo pra cantar
+  
+  [Refrão]
+  
+  <b>Dm</b>              <b>C</b>
+    Eu só peço a Deus
+            <b>F</b>        <b>G</b>
+  Um pouco de malandragem
+  <b>Dm</b>            <b>C</b>
+    Pois sou criança
+            <b>F</b>         <b>G</b>
+  E não conheço a verdade
+  <b>Bb5(9)</b>     <b>C</b>                         <b>Dm</b>
+        Eu sou poeta e não aprendi a amar
+  <b>Bb5(9)</b>     <b>C</b>                         <b>Dm</b>
+        Eu sou poeta e não aprendi a amar
+  
+                                  <b>Dm</b>
+  Quem sabe ainda sou uma garotinha</pre>  
+
+  <style>
+    html {
+      scroll-behavior: smooth;
+    }
+
+    body {
+      position: relative;
+      font-size: 40px;
+    }
+
+    b {
+      color: #d2326f
+    }
+
+    .buttonsContainer {
+      position: absolute;
+      right: 0;
+      top: 20px;
+      display: flex;
+      flex-direction: column;
+      gap: 40px
+    }
+
+    button {
+      font-size: 30px
+    }
+  </style>
+  `
+
+  const webViewScroll = () => {
+    if (isScrolling) {
+      setDelay(null)
+      setIsScrolling(false)
+      return
+    }
+
+    setDelay(500)
+    setIsScrolling(true)
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <StatusBar style='auto' translucent />
+
+      <WebView ref={webViewRef} source={{ html: tab }} showsVerticalScrollIndicator={false} originWhitelist={['*']} />
+
+      <View style={styles.rollButton}>
+        <TouchableOpacity onPress={webViewScroll}>
+          <Text>{isScrolling ? 'stop' : 'scroll'}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 25
+  },
+  rollButton: {
+    position: 'absolute',
+    top: 50,
+    right: 50,
+    elevation: 5,
+    zIndex: 1,
+    padding: 10,
+    backgroundColor: '#a4ffff'
+  }
+});
